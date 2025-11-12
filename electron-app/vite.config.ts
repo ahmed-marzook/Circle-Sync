b4bdfb161089cfc65a7ce866a61a09978292bc17
Ahmed Marzook <m4marzook@gmail.com>
2025-11-12 15:39:36 +0000

    Electron and react setup

---
+++ electron-app/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    devtools(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: 'src/ui/routes',
+      generatedRouteTree: 'src/ui/routeTree.gen.ts',
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: './',
+  build: {
+    outDir: 'dist-react',
+  },
+})
